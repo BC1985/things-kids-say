511e3757bba086cdd9aaf40c62a7dad9f717cf32
Ben Cohen <benjcohen1985@gmail.com>
2020-03-02 10:30:22 -0500

    Import components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ContextProvider } from "./Context";
+import Nav from "./components/Nav/Nav";
+import AddQuote from "./components/AddQuote/AddQuote";
+import FullList from "./components/FullList/FullList";
+import LandingText from "./components/LadingPage/LandingPage";
+import RandomQuote from "./components/RandomQuote/RandomQuote";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <ContextProvider>
+          <Route exact path="/" component={LandingText} />
+          <Route path="/list" component={FullList} />
+          <Route path="/random" component={RandomQuote} />
+          <Route path="/add" component={AddQuote} />
+        </ContextProvider>
+      </Switch>
+    </Router>
   );
 }
 
