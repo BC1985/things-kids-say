982f9e5327022dce631ea58e3dd218e71c69aba7
Ben Cohen <benjcohen1985@gmail.com>
2020-08-15 12:42:18 +0300

    Format code

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -5,7 +5,6 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  
 
   const getToken = async (email, password) => {
     try {
@@ -29,22 +28,28 @@ function Login() {
     <div className="container">
       <h1 className="mt-3 childish-font">Login</h1>
       <form className="form-group p-3" onSubmit={handleSubmit}>
-          <label className="col-form-label">Email</label>
+        <label className="col-form-label">Email</label>
         <input
-            className="form-control"
+          className="form-control"
           type="text"
           value={email}
           onChange={e => setEmail(e.target.value)}
         />
         <label className="col-form-label">Password</label>
         <input
-        className="form-control"
+          className="form-control"
           type="text"
           value={password}
           onChange={e => setPassword(e.target.value)}
         />
-        <p className="mt-2 text-danger">{error ?error : " "}</p>
-        <button type="submit" className="btn btn-outline-primary font-weight-bold col-sm-4 mt-3" disabled={!isEnabled}>Submit</button>
+        <p className="mt-2 text-danger">{error ? error : " "}</p>
+        <button
+          type="submit"
+          className="btn btn-outline-primary font-weight-bold col-sm-4 mt-3"
+          disabled={!isEnabled}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
