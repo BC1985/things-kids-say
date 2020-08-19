7ed7a7e55ed9febbfa8d4c9e9c67bde13846f39a
Ben Cohen <benjcohen1985@gmail.com>
2020-08-19 12:49:09 +0300

    Include token in headers on post request

--- src/Services/apiServices.js
+++ src/Services/apiServices.js
@@ -1,18 +1,21 @@
 export const apiService = {
-  addNewEntry(entry) {
-    return fetch("http://localhost:5000/sayings/add", {
+  addNewEntry: async entry=> {
+    const token = localStorage.getItem("jwt token")
+    const res= await fetch("http://localhost:5000/sayings/add", {
       method: "POST",
       headers: {
         "content-type": "application/json",
+        "authorization": `bearer ${token}`
       },
       body: JSON.stringify(entry),
-    }).then(res => {
-      if (!res.ok) {
-        res.json().then(e => Promise.reject(e));
-      } else {
-        res.json();
-      }
     });
+    const data = await res.json()
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    else{
+      return data
+    }
   },
 
   postUser: async user => {
