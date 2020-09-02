d41efee84721f8da9fa4c2798822dfc5c768b32a
Ben Cohen <benjcohen1985@gmail.com>
2020-09-02 09:53:10 +0300

    Display edit link on hover

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import "./Quote.css";
+
+const editIcon = (
+  <FontAwesomeIcon icon={faPencilAlt} className="text-danger edit-icon ml-3" />
+);
 
 const Quote = ({ sayings }) => {
   return sayings.map(quote => {
     return (
       <li key={quote._id} style={{ listStyleType: "none" }} className="mb-5">
-        <h2>"{quote.content}"</h2>
+        <div className="item d-flex">
+          <h2 className="item">"{quote.content}"</h2>
+          <Link to={`/edit/${quote._id}`}>{editIcon}</Link>
+        </div>
         <h3 style={{ color: "gray" }}>
           {quote.kid_name}, age {quote.age}
         </h3>
