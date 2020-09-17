720b734dbba353b236a2b80efc8a9392f7359005
Ben Cohen <benjcohen1985@gmail.com>
2020-09-17 17:36:00 +0300

    Display username in nav Add my quotes link when signed in

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import {withRouter} from "react-router"
+import { context } from "../../Context";
+
 import { Link } from "react-router-dom";
 import "./Nav.css";
 function Nav(props) {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("jwt token");
+
+      const res = await fetch(`http://localhost:5000/users/username`, {
+        method: "GET",
+        headers: {
+          authorization: `bearer ${token}`,
+        },
+      });
+      let data = await res.json();
+      setUser(data);
+      // let username = data._id
+    };
+    fetchData();
+  }, []);
+
   const logoStyle = {
     background: "teal",
     textAlign: "center",
@@ -15,6 +36,7 @@ function Nav(props) {
         <Link className="nav-link link" to="/">
           <h1>Things Kids Say</h1>
         </Link>
+        <p>Welcome {user.username}</p>
         <ul className="nav justify-content-end d-flex align-content-center">
           <li className="nav-item">
             <Link to="/list" className="nav-link link">
@@ -24,6 +46,14 @@ function Nav(props) {
           <li>
             <Link to={isSignedIn? 'add':'login'} className="nav-link link">
               {isSignedIn ? 'Add Quote': 'Log in'}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={isSignedIn ? `my_quotes/user/${user._id}` : ""}
+              className="nav-link link"
+            >
+              {isSignedIn ? "My Quotes" : ""}
             </Link>
           </li>
           <li>
