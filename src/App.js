dfb87f7611e433c9762e97a1d4609744f96af5c0
Ben Cohen <benjcohen1985@gmail.com>
2020-09-19 16:31:53 +0300

    Format code

--- src/App.js
+++ src/App.js
@@ -6,37 +6,35 @@ import { ContextProvider } from "./Context";
 import Nav from "./components/Nav/Nav";
 import AddQuote from "./components/AddQuote/AddQuote";
 import FullList from "./components/FullList/FullList";
-import LandingPage from "./components/LandingPage/LandingPage"
+import LandingPage from "./components/LandingPage/LandingPage";
 import RandomQuote from "./components/RandomQuote/RandomQuote";
 import SignIn from "./components/Sign-In/Sign-in";
 import Login from "./components/Login/login";
-import  ErrorBoundary from "./components/ErrorBoundary";
-import EditQuote from "./components/EditQuote/EditQuote"
-import MyQuotes from "./components/MyQuotes/MyQuotes"
+import ErrorBoundary from "./components/ErrorBoundary";
+import EditQuote from "./components/EditQuote/EditQuote";
+import MyQuotes from "./components/MyQuotes/MyQuotes";
 import SideNav from "./components/Sidenav/Sidenav";
 
 function App() {
-
-  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [isSignedIn, setIsSignedIn] = useState(false);
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
 
   useEffect(() => {
-    const userAlreadyLoggedIn = localStorage.getItem('jwt token')
+    const userAlreadyLoggedIn = localStorage.getItem("jwt token");
     if (userAlreadyLoggedIn) {
-        setIsSignedIn(true)            
+      setIsSignedIn(true);
     }
   }, []);
 
-  const logInUser = () =>{
-    setIsSignedIn(true)    
-  }
+  const logInUser = () => {
+    setIsSignedIn(true);
+  };
 
-  const logOut = () =>{
-    setIsSignedIn(false)
-    localStorage.removeItem('jwt token')
-    toggleSideNav()
-    
-  }
+  const logOut = () => {
+    setIsSignedIn(false);
+    localStorage.removeItem("jwt token");
+    toggleSideNav();
+  };
   const toggleSideNav = () => {
     setIsSideNavOpen(!isSideNavOpen);
   };
@@ -47,31 +45,44 @@ function App() {
     isSideNavOpen,
     toggleSideNav,
     isSignedIn,
-  }
-  const loginProps ={
+  };
+  const loginProps = {
     isSignedIn,
-    logInUser
-  }
+    logInUser,
+  };
 
   return (
     <Router>
-      <ErrorBoundary>      
-      <Switch>
-        <ContextProvider>
-          <SideNav {...navProps}/>
-          <Nav {...navProps}/>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/list" component={FullList} />
-          <Route path="/edit/:id" render={props=>(<EditQuote {...props}/>)} />
-          <Route path="/random" component={RandomQuote} />
-          <Route path="/add" render={props=>{ return isSignedIn ? <AddQuote {...props} isSignedIn={isSignedIn} /> : <Login logInUser={logInUser} /> }}/>
-          <Route path="/signup" 
-            render={()=>(<SignIn {...loginProps} />)}/>
-          <Route path="/login"
-            render={()=>(<Login {...loginProps}/>)}/>
-          <Route path="/my_quotes/user/:id" render={props=><MyQuotes {...props} isSignedIn={isSignedIn}/>}/>
-        </ContextProvider>
-      </Switch>
+      <ErrorBoundary>
+        <Switch>
+          <ContextProvider>
+            <SideNav {...navProps} />
+            <Nav {...navProps} />
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/list" component={FullList} />
+            <Route
+              path="/edit/:id"
+              render={props => <EditQuote {...props} />}
+            />
+            <Route path="/random" component={RandomQuote} />
+            <Route
+              path="/add"
+              render={props => {
+                return isSignedIn ? (
+                  <AddQuote {...props} isSignedIn={isSignedIn} />
+                ) : (
+                  <Login logInUser={logInUser} />
+                );
+              }}
+            />
+            <Route path="/signup" render={() => <SignIn {...loginProps} />} />
+            <Route path="/login" render={() => <Login {...loginProps} />} />
+            <Route
+              path="/my_quotes/user/:id"
+              render={props => <MyQuotes {...props} isSignedIn={isSignedIn} />}
+            />
+          </ContextProvider>
+        </Switch>
       </ErrorBoundary>
     </Router>
   );
