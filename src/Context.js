93227b56aedaa3faad8fea90c7ae3d9000011f6f
Ben Cohen <benjcohen1985@gmail.com>
2020-09-19 08:50:22 +0300

    Get username

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, createContext } from "react";
 import axios from "axios";
+import { apiService } from "./Services/apiServices"
 require('dotenv').config()
 
 const context = createContext();
@@ -7,6 +8,7 @@ const context = createContext();
 function ContextProvider({ children }) {
   const [sayings, setSayings] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState({});
 
   const uri = 'http://localhost:5000/sayings';
 
@@ -18,11 +20,20 @@ function ContextProvider({ children }) {
       setSayings(res.data);
       setIsLoading(false);
     };
+    const fetchUsername = async()=>{
+      const res = await apiService.getUsername()
+      if (res === undefined) {
+        return null
+      }else{
+        setUser(res)
+      }
+    }
     fetchData();
+    fetchUsername()
   }, []);
 
   return (
-    <context.Provider value={{ sayings, isLoading }}>
+    <context.Provider value={{ sayings, isLoading, user }}>
       {children}
     </context.Provider>
   );
