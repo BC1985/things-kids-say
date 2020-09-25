867f7fe4b4011f6ce0f7e7a40996a9a1a22ec8c9
Ben Cohen <benjcohen1985@gmail.com>
2020-09-25 09:18:36 +0300

    Get user quotes array inside hook Display number of quotes

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,19 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { context } from "../../Context";
 import { Link } from "react-router-dom";
+import { apiService } from "../../Services/apiServices";
+import Spinner from "../Spinner/Spinner";
 
 function Settings(props) {
   const { isSignedIn } = props;
-  const { user } = useContext(context);
+  const { user, sayings } = useContext(context);
+  const [quotesByUser, setQuotesByUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+    const fetchData = async () => {
+      let id = props.match.params.id;
+      const userQuotes = await apiService.getQuotesByUser(id);
+      setQuotesByUser(userQuotes);
+    };
+    fetchData();
+  }, [sayings, props.match.params.id]);
+
 
   return (
     <div className="container p-3">
       <h1>Settings</h1>
-      {isSignedIn && (
-        <Link to={isSignedIn ? `/my_quotes/user/${user._id}` : ""}>
-          My quotes
-        </Link>
-      )}
+      <div className="mt-3">
+        {isSignedIn && (
+          <Link to={`/my_quotes/user/${user._id}`}>My quotes </Link>
+        )}
+        <span>{isLoading ? <Spinner /> : quotesByUser.length}</span>
+      </div>
     </div>
   );
 }
