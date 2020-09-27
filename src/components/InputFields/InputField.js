0e762b95ace1ebc562abaa87c9cd3d0012867381
Ben Cohen <benjcohen1985@gmail.com>
2020-09-27 13:18:51 +0300

    Create input field component

---
+++ src/components/InputFields/InputField.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const InputField = props => {
+  return (
+    <div>
+      <label htmlFor={props.name} className="col-form-label childish-font">
+        {props.title}
+      </label>
+      <input
+        validation={props.validation}
+        title={props.title}
+        className="form-control shadow-sm mb-3"
+        placeholder={props.placeholder}
+        name={props.name}
+        value={props.value}
+        onChange={props.onChange}
+        type={props.type}
+      />
+    </div>
+  );
+};
+
+InputField.defaultProps = {
+  placeholder: "",
+  name: "",
+  type: "text",
+  value: "",
+};
+
+export default InputField;
