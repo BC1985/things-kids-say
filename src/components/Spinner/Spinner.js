bbc4e905c2dc3cef7e2072fb7ab16310039cfb31
Ben Cohen <benjcohen1985@gmail.com>
2020-10-05 11:56:29 +0300

    Fix syntax

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Spinner() {
     return (
-        <div className="spinner-grow text-primary" role="status">
+        <span className="spinner-grow text-primary" role="status">
           <span className="sr-only">Loading...</span>
-        </div>
+        </span>
       );
 }
 
