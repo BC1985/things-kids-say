b729e66466f53807624d08e384dd49618cfb2b9a
Ben Cohen <benjcohen1985@gmail.com>
2021-04-17 16:09:49 +0300

    Revert to old pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,34 +1,26 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronRight,
-  faChevronLeft,
-} from "@fortawesome/free-solid-svg-icons";
-
-const icons = {
-  right: <FontAwesomeIcon icon={faChevronRight} className="ml-3" />,
-  left: <FontAwesomeIcon icon={faChevronLeft} className="ml-3" />,
-};
-function Pagination({ pages, page, changePage }) {
+import { Link } from "react-router-dom";
+function Pagination({ quotesPerPage, totalQuotes, paginate}) {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalQuotes / quotesPerPage); i++) {
+    pageNumbers.push(i);
+  }
   return (
-    pages > 1 && (
+    <nav>
       <ul className="pagination">
-        <button
-          className="btn"
-          onClick={() => changePage(page => parseInt(page) - 1)}
-          disabled={page === 1}
-        >
-          {icons.left}
-        </button>
-        <button
-          className="btn"
-          onClick={() => changePage(page => parseInt(page) + 1)}
-          disabled={page === pages}
-        >
-          {icons.right}
-        </button>
+        {pageNumbers.map(number => (
+          <li key={number} className="page-item">
+            <Link
+              to={`/quotes/${number}`}
+              onClick={() => paginate(number)}
+              className="page-link"
+            >
+              {number}
+            </Link>
+          </li>
+        ))}
       </ul>
-    )
+    </nav>
   );
 }
 
