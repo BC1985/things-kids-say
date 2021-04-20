e1414af54140b2570a5d119b9862cff6f2ec8927
Ben Cohen <benjcohen1985@gmail.com>
2021-04-20 14:44:42 +0300

    Fix typo

--- src/helpers.js
+++ src/helpers.js
@@ -12,6 +12,6 @@ export const icons = {
   check: <FontAwesomeIcon icon={faCheckCircle} className="ml-3" />,
   abort: <FontAwesomeIcon icon={faTimesCircle} className="ml-3" />,
   edit: <FontAwesomeIcon icon={faPencilAlt} className="text-danger edit-icon ml-3"/>,
-  makeVisibile: <FontAwesomeIcon icon={faEye} className="ml-3"/>,
-  makeInvisibile: <FontAwesomeIcon icon={faEyeSlash} className="ml-3"/>
+  makeVisible: <FontAwesomeIcon icon={faEye} />,
+  makeInvisible: <FontAwesomeIcon icon={faEyeSlash} />
 };
